8662fee52b31b48aec6f1135f2979efec5d2da8d
Владимир К <admin@chertenok.ru>
2022-11-24 01:42:19 +0300

    FIx to HomeWork 2

--- Assets/Scripts/COP/JumpComponent.cs
+++ Assets/Scripts/COP/JumpComponent.cs
@@ -9,17 +9,11 @@ namespace Components
         [SerializeField]
         [Required]
         private EventReceiver receiverTargetJump;
-        [SerializeField]
-        [Required]
-        private IntBehaviour jumpForce;
+
         public void Jump()
         {
             receiverTargetJump.Call();
         }
 
-        public int GetJumpForce()
-        {
-            return jumpForce.Value;
-        }
     }
 }

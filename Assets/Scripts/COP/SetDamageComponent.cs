1f37e57cfaebc94afe71d9c89381f65da7b7f8e5
Владимир К <admin@chertenok.ru>
2022-11-23 04:17:43 +0300

    Бонусы к оружию

---
+++ Assets/Scripts/COP/SetDamageComponent.cs
@@ -0,0 +1,19 @@
+﻿
+using Mechanics;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Components
+{
+    public class SetDamageComponent : MonoBehaviour, ISetDamageComponent
+    {
+        [SerializeField]
+        [Required]
+        private IntBehaviour damageField;
+
+        public void SetDamage(int damage)
+        {
+            damageField.Value = damage;
+        }
+    }
+}
---
+++ Assets/Scripts/Mechanics/CreatePrefabMechanics.cs
@@ -0,0 +1,32 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Mechanics
+{
+    public sealed class CreatePrefabMechanics : MonoBehaviour
+    {
+        [SerializeField]
+        [Required]
+        private EventReceiver createSourceReciver;
+        [SerializeField]
+        [Required]
+        private GameObject prefab;
+        [SerializeField]
+        [Required]
+        private Transform createPosition;
+
+        private void OnEnable()
+        {
+            createSourceReciver.OnEvent += OnCreate;
+        }
+        private void OnDisable()
+        {
+            createSourceReciver.OnEvent -= OnCreate;
+        }
+
+        void OnCreate()
+        {
+            Instantiate(prefab, createPosition.position, Quaternion.identity);
+        }
+    }
+}
